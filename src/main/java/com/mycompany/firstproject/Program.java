b14384c9a455de2aead2717458491e7828712729
YangZhang <1904077@student.uwtsd.ac.uk>
2019-10-25 11:48:00 +0000

    1

--- src/main/java/com/mycompany/firstproject/Program.java
+++ src/main/java/com/mycompany/firstproject/Program.java
@@ -21,14 +21,14 @@ public class Program
         //Your job is to fix the errors and finish them.
         
         //Print "Hello, World" to console
-        System.out.printf(Hello, World)
+        System.out.printf("Hello, World");
         
         //Read two numbers, then print the sum of them
         System.out.printf("Please enter first number: ");
-        int num1 = Integer.parseInt(stdin.nextline());
+        int num1 = Integer.parseInt(stdin.nextLine());
         
-        System.out.printf("Please enter first number: ");
-        int num1 = Integer.parseInt(stin.nextLine());
+        System.out.printf("Please enter second number: ");
+        int num2 = Integer.parseInt(stdin.nextLine());
         
         int result = num1 + num2;
         
